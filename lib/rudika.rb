d96cd1b767da7930ffff4d16e5df8fdf2714eb4c
harupong <harupong@gmail.com>
2013-11-05 17:33:31 +0900

    Implement `rudika list` command
    this commmand lists all available stations for recording
    
    TODO: transfer stations.rb, which holds the lists, to some other file
    format

--- lib/rudika.rb
+++ lib/rudika.rb
@@ -1,26 +1,38 @@
 #!/usr/bin/env ruby
 
 require "thor"
-#require "./lib/commands"
+require "./lib/stations"
 
 class Rudika < Thor
+
+  @@nhk_list = get_nhk_stations
+  @@radiko_list = get_radiko_stations
+
   desc "rec", "rip and record the stream from Radiko/NHK"
   option :station, :required => true, :aliases => "-s"
-    def rec
-      station = options[:station]
+  def rec
+    nhk = @@nhk_list.keys
+    radiko = @@radiko_list.keys
+    station = options[:station]
 
-      case station
-      when "NHK1", "NHK2", "FM"
-        system ("ripdiru #{station}")
-      else
-        system ("ripdiko #{station}")
-      end
+    case station
+    when *nhk
+      system ("ripdiru #{station}")
+    when *radiko
+      system ("ripdiko #{station}")
+    else
+      puts "invalid station name. try `list` command."
     end
+  end
 
   desc "list", "lists all available stations"
-    def list
-      puts "listing..."
+  def list
+    all_stations = @@nhk_list.merge(@@radiko_list)
+
+    all_stations.each.with_index(1) do |a, id|
+      puts "#{id}: #{a}"
     end
+  end
 
   desc "schedule", "schedules a recording"
     def schedule
