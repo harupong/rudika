d660b47aad7f248b5bdd4448c612853c6568dfe2
harupong <harupong@gmail.com>
2013-11-06 15:43:35 +0900

    Imprement `rudika schedule --add` feature
    this prompts and adds a recording schedule, which is passed to
    `whenever` command and gets added to user's crontab.

--- lib/rudika.rb
+++ lib/rudika.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
-require "thor"
-require "./lib/stations"
+require 'thor'
+require 'date'
+require './lib/stations'
 
 class Rudika < Thor
 
@@ -38,8 +39,64 @@ class Rudika < Thor
   end
 
   desc "schedule", "schedules a recording"
+  option :add, :type => :boolean, :aliases => "-a"
+  option :delete, :type => :boolean, :aliases => "-d"
+  option :show, :type => :boolean, :default => true, :aliases => "-s"
   def schedule
-    puts "scheduling..."
+    case
+    when options[:add]
+      add_schedule
+    when options[:delete]
+      delete_schedule
+    when options[:show]
+      system ("whenever -f")
+    end
   end
 
+  protected
+
+  def add_whenever_config(str)
+    File.open("config/schedule.rb","a") do |file|
+        file.write("#{str}\n")
+    end
+  end
+
+  def add_schedule
+    print "Enter the station:"
+      station = STDIN.gets.chomp.upcase
+    print "Enter the frequency. Daily, Weekly, Monthly[D,W,M]:"
+      frequency = STDIN.gets.chomp.upcase
+    print "Enter the date[YYYY-MM-DD hh:mm]:"
+      inputtime = STDIN.gets.chomp
+      t = DateTime.strptime("#{inputtime}:00+0900", '%Y-%m-%d %H:%M:%S%z')
+      dayofmonth = t.day
+      monthname =Date::MONTHNAMES[t.month]
+      dayname = Date::DAYNAMES[t.wday]
+      starttime = "#{t.hour}:#{t.minute}"
+
+    case frequency
+    when "D"
+      str = "every 1.day, :at => '#{starttime}' do command \"rudika rec -s #{station}\" end"
+    when "W"
+      str = "every :#{dayname}, :at => '#{starttime}' do command \"rudika rec -s #{station}\" end"
+    when "M"
+      # must pass monthname to :at symbol so that `whenever` can parse it correctly
+      # https://github.com/javan/whenever/issues/13#issuecomment-18869
+      str = "every 1.month, :at => '#{monthname} #{dayofmonth} #{starttime}' do command \"rudika rec -s #{station}\" end"
+    end
+
+    add_whenever_config(str)
+    system ("whenever -i")
+  end
+
+  def delete_schedule
+    print "are you sure to delete schedule?[Yn]:"
+      delete_confirmation = STDIN.gets.chomp
+      case delete_confirmation.upcase
+      when "Y"
+        puts "YES"
+      else
+        puts "NO"
+      end
+  end
 end
